48ef5e25a1dffc300a5dae04cffcaa34ff489fe2
Dasha05896 <Budovich_Dasha@ukr.net>
2024-10-31 18:54:40 +0200

    Limit FuncA to first 3 terms

--- Exponential.cpp
+++ Exponential.cpp
@@ -1,5 +1,11 @@
 #include "Exponential.h"
 
 double Exponential::FuncA(int n) {
-    return 0; // Initial implementation returning 0
+    double sum = 1.0; // Initial member (x^0 / 0!) = 1
+    double term = 1.0;
+    for (int i = 1; i < 3; ++i) { // Only the first 3 elements
+        term *= 1.0 / i; // Let's count (x^i / i!)
+        sum += term;
+    }
+    return sum;
 }
