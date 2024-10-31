883adaa01abcee9abf8ec357dc3d9f476a6e4da5
Dasha05896 <Budovich_Dasha@ukr.net>
2024-10-31 18:45:47 +0200

    Implement FuncA for Taylor series sum

--- Exponential.cpp
+++ Exponential.cpp
@@ -1,5 +1,12 @@
 #include "Exponential.h"
+#include <cmath>
 
 double Exponential::FuncA(int n) {
-    return 0; // Initial implementation returning 0
+    double sum = 1.0; // Initial member (x^0 / 0!) = 1
+    double term = 1.0;
+    for (int i = 1; i < n; ++i) {
+        term *= 1.0 / i; // Let's count (x^i / i!)
+        sum += term;
+    }
+    return sum;
 }
