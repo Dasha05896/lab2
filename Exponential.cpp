3c707e3c839c0fac70b47ea915243ba807fe9e6c
Dasha05896 <Budovich_Dasha@ukr.net>
2024-10-31 18:59:33 +0200

    Complete FuncA implementation with n terms

--- Exponential.cpp
+++ Exponential.cpp
@@ -1,11 +1,12 @@
 #include "Exponential.h"
-#include <cmath>
 
+// FuncA calculates the sum of the first n elements of the Taylor series for e^x.
+// The n parameter specifies the number of members to be included in the sum.
 double Exponential::FuncA(int n) {
     double sum = 1.0; // Initial member (x^0 / 0!) = 1
     double term = 1.0;
-    for (int i = 1; i < n; ++i) {
-        term *= 1.0 / i; // Let's count (x^i / i!)
+    for (int i = 1; i < n; ++i) { // Calculation of the summation of the first n terms
+        term *= 1.0 / i;
         sum += term;
     }
     return sum;
